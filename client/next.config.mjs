c3175d87d555d01fb147df6fdae8f517de989c19
Akhabue Ehime Emmanuel <emmexanimation@gmail.com>
2025-12-10 11:10:15 +0100

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['res.cloudinary.com'], // Allow Cloudinary images
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
   },
 };
 
